598271f6bde6f29305b95475b9715c85c34694d7
fearlessalan <fearlessalain@gmail.com>
2025-10-27 18:13:09 +0200

    Resolving the build error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { App } from './app/app';
 
 bootstrapApplication(App, {
-  providers: [provideRouter([]), provideAnimationsAsync()],
+  providers: [provideRouter([])],
 }).catch((err) => console.error(err));
